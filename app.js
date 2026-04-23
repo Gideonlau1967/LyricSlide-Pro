d1bd97ae0f77bd84458fe789302444f7ab1b384d
Gideonlau1967 <gideonlau@gmail.com>
2026-04-23 08:38:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* LyricSlide Pro */
 
 const App = {
-    version: "2.2.4",
+    version: "2.2.5",
     elements: {
         songTitle: document.getElementById('songTitle'),
         lyricsInput: document.getElementById('lyricsInput'),
